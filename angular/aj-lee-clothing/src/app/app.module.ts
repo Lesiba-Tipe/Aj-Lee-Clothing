6c35029eb97c576c3e0e19fb031bd4c4605b10b7
Lesiba-Tipe <Lesiba-Tipe@users.noreply.github.com>
2021-03-01 06:20:01 +0200

    added home and about components

--- angular/aj-lee-clothing/src/app/app.module.ts
+++ angular/aj-lee-clothing/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about/about.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
